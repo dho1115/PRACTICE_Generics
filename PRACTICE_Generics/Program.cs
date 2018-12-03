61eb16e7ca82a096b882c087642bba5f2724f69f
DAVID HO <davidhorph@hotmail.com>
2018-12-02 20:59:23 -0600

    gfdgfsd

--- PRACTICE_Generics/Program.cs
+++ PRACTICE_Generics/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PRACTICE_Generics
 {
@@ -6,7 +7,31 @@ namespace PRACTICE_Generics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<RandomObjects<object>> MyRandomList = new List<RandomObjects<object>>()
+            {
+                new RandomObjects<object>(),
+                new RandomObjects<object>(5),
+                new RandomObjects<object>(true)
+            };
+            
+            Console.ReadLine();
+        }
+    } 
+
+    class RandomObjects<T>
+    {
+        public T obj { get; set; }
+
+        public RandomObjects()
+        {
+
+        }
+
+        public RandomObjects(T _obj)
+        {
+            this.obj = _obj;
+
+            Console.WriteLine(this.obj);
         }
     }
 }
